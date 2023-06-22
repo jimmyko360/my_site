c29d4baf60e20f4e3a1ec5f4448d69d717a1ff9a
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-22 19:56:47 -0400

    created all_posts view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+posts = [
+    {
+        "title": "my first post",
+        "body": "hooray",
+        "date": datetime.datetime(2023, 3, 1)
+    },
+    {
+        "title": "my second post",
+        "body": "take two",
+        "date": datetime.datetime(2023, 4, 1)
+    },
+    {
+        "title": "my third post",
+        "body": "wassup chat",
+        "date": datetime.datetime(2023, 5, 1)
+    },
+]
+
+def all_posts(request):
+  titles = []
+
+  for post in posts:
+    titles.append(post["title"])
+
+  return render(request, "blog/posts.html", {"titles": titles})
