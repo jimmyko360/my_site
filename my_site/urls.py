a622b0292c6afbf37251f26979d1b66ff62e7de6
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-23 02:03:11 -0400

    registered homepage app's urls

--- my_site/urls.py
+++ my_site/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include("blog.urls"))
+    path('posts/', include("blog.urls")),
+    path('', include("homepage.urls"))
 ]
