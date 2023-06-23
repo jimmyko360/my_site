4d856826ee9096150abf821dba564e2144902bb7
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-23 00:19:44 -0400

    added endpoint and view for an individual blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("<str:slug>", views.post_by_title, name="post_by_title"),
     path("", views.all_posts, name="all_posts")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,40 @@
 from django.shortcuts import render
 import datetime
+from django.http import Http404
 
 posts = [
     {
         "title": "my first post",
-        "body": "hooray",
+        "slug": "my-first-post",
+        "body": "wassup chat",
         "date": datetime.datetime(2023, 3, 1)
     },
     {
         "title": "my second post",
-        "body": "take two",
+        "slug": "my-second-post",
+        "body": "its ya boi jkizzle",
         "date": datetime.datetime(2023, 4, 1)
     },
     {
         "title": "my third post",
-        "body": "wassup chat",
+        "slug": "my-third-post",
+        "body": "live on twitch.tv",
         "date": datetime.datetime(2023, 5, 1)
     },
 ]
+
+def post_by_title(request, slug):
+  try:
+    blog_post = {}
+
+    for post in posts:
+      if slug == post["slug"]:
+        blog_post = post
+
+    # if you want pass multiple values to the template, you should break them up into separate dict properties here because you can't use key-value pair references in template interpolation
+    return render(request, "blog/blog_post.html", {"title": blog_post["title"], "body": blog_post["body"]})
+  except:
+    raise Http404()
 
 def all_posts(request):
   titles = []
