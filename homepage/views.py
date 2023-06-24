53f2d977e4ae43b13e702526998c076bfcb2d38b
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-24 02:46:48 -0400

    used try/except block to raise Http404 in the case of an error

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound, Http404
 from data import posts
 
 def index(request):
   # using a try/except block here was causing an error, why?
-  return render(request, "homepage/index.html", {"blog_post": posts[0]})
+  # it's because I was returning Http404 instead of raising it
+  try:
+    return render(request, "homepage/index.html", {"blog_post": posts[0]})
+  except:
+    raise Http404()
