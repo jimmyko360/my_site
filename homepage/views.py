e0620830aad43b8422573d246eec52368382406b
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-24 18:44:52 -0400

    converted post title to slug in index view and passed it to the template

--- homepage/views.py
+++ homepage/views.py
@@ -6,6 +6,8 @@ def index(request):
   # using a try/except block here was causing an error, why?
   # it's because I was returning Http404 instead of raising it
   try:
-    return render(request, "homepage/index.html", {"blog_post": posts[0]})
+    slug = posts[0]["title"].replace(" ", "-")
+
+    return render(request, "homepage/index.html", {"blog_post": posts[0], "slug": slug})
   except:
     raise Http404()
