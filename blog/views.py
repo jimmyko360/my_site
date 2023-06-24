7163f99303fa5ed5a26d22357e2ab97a81c0f5b3
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-24 02:46:03 -0400

    fixed error handling to raise an Http404 error that properly references the 404 template

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,15 @@ from data import posts
 # you don't have to navigate to the data file with ..'s, weird
 
 def post_by_title(request, slug):
-  try:
-    blog_post = {}
+  blog_post = None
 
-    # key-value reference MUST use bracket notation in this file for some reason, e.g. in this view and the all_posts view
-    for post in posts:
-      if slug == post["slug"]:
-        blog_post = post
-
+  # key-value reference MUST use bracket notation in this file for some reason, e.g. in this view and the all_posts view
+  for post in posts:
+    if slug == post["slug"]:
+      blog_post = post
+  if blog_post:
     return render(request, "blog/blog_post.html", {"blog_post": blog_post})
-  except:
+  else:
     raise Http404()
 
 def all_posts(request):
@@ -28,4 +27,4 @@ def all_posts(request):
 
     return render(request, "blog/posts.html", {"titles": titles})
   except:
-    return Http404()
+    raise Http404()
