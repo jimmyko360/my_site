1540ce7e323f98497d587a7f7e41a8df2972fe80
Jimmy Ko <jimmy.ko@nyu.edu>
2023-06-24 18:43:38 -0400

    removed slug property from post objects

--- data.py
+++ data.py
@@ -2,20 +2,20 @@ import datetime
 
 posts = [
     {
-        "title": "my third post",
-        "slug": "my-third-post",
+        "title": "jimmy's third post",
+        # "slug": "my-third-post",
         "body": "live on twitch.tv",
         "date": datetime.datetime(2023, 5, 1)
     },
     {
         "title": "my second post",
-        "slug": "my-second-post",
+        # "slug": "my-second-post",
         "body": "its ya boi jkizzle",
         "date": datetime.datetime(2023, 4, 1)
     },
     {
         "title": "my first post",
-        "slug": "my-first-post",
+        # "slug": "my-first-post",
         "body": "wassup chat",
         "date": datetime.datetime(2023, 3, 1)
     }
